979defb56c0969530ba787a3881782544694fdfd
Nguyen_Le_Khoi <khoinlse183712@fpt.edu.vn>
2025-02-14 13:43:23 +0700

    Add Listing-page

--- fe/src/router/Router.js
+++ fe/src/router/Router.js
@@ -3,6 +3,10 @@ import Login from '../pages/Login/Login'
 import Home from '../pages/Homepage/Homepage'
 import ButtonAppBar from '../layouts/Navbar'
 import Footer from '../layouts/Footer'
+import ListingPage from '../pages/Listingpage/Listingpage'
+
+
+
 
 const Layout = ({ children, showHeader = true, showFooter = true}) => {
     return (
@@ -28,6 +32,14 @@ const Layout = ({ children, showHeader = true, showFooter = true}) => {
       element: (
         <Layout showHeader={false} showFooter={false}>
           <Login />
+        </Layout>
+      ),
+    },
+    {       
+      path: '/listing-page',
+      element: (
+        <Layout showHeader={false} showFooter={false}>
+         <ListingPage/>
         </Layout>
       ),
     },
