fd19b6e780168ae88345c407450e4ea6a18602b0
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-03-25 17:00:07 +0700

    giang/test deploy

--- fe/src/components/Loading/LoadingScreen.js
+++ fe/src/components/Loading/LoadingScreen.js
@@ -37,7 +37,7 @@ const LoadingScreen = () => {
           fontFamily: 'Yusei Magic',
         }}
       >
-        Loading BlindBoxx...
+        Loading BlindBoxx...!
       </Typography>
     </Box>
   );
